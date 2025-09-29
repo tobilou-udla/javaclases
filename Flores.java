6da3f7d6254dbca8c62116995b32f34b9a964d04
Jaime Hidalgo <jaime.hidalgo.barroso@udla.edu.ec>
2025-09-29 10:34:38 -0500

    Create Flores class with properties and method

---
+++ Flores.java
@@ -0,0 +1,14 @@
+public class Flores {
+    String color;
+    String nombre;
+    double precio;
+    int stock;
+
+    public void detalle(){
+        System.out.println("Nombre: "+ this.nombre);
+        System.out.println("Color: "+ this.color);
+        System.out.println("Precio: "+ this.precio);
+        System.out.println("Stock: "+ this.stock);
+    }
+
+}
