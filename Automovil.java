0823de44351242cea5377cf2d82a85ee2d3f307a
Jaime Hidalgo <jaime.hidalgo.barroso@udla.edu.ec>
2025-09-29 10:35:02 -0500

    Create Automovil class with details method

---
+++ Automovil.java
@@ -0,0 +1,17 @@
+public class Automovil {
+    String marca;
+    String modelo;
+    String color="Blanco";
+    double cilindraje;
+
+    //System.out.println("Info del metodo");
+    //METDO PARA IMPRIIR
+    public void detalle(){
+        String modelo ="AUDI";
+        System.out.println("auto1.marca: "+ this.marca);
+        System.out.println("auto1.modelo: "+ this.modelo);
+        System.out.println("auto1.color: "+ this.color);
+        System.out.println("auto1.cilindraje: "+ this.cilindraje);
+    }
+
+}
