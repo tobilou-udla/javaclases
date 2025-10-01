92bc694bd197d09bff58ed2f4e29d6e572b0990d
Jaime Hidalgo <jaime.hidalgo.barroso@udla.edu.ec>
2025-10-01 08:03:27 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Automovil auto1 = new Automovil("bmw","I320","blanco",3.2);
         // Constructor
         /*/ASIGNAR VALORES
@@ -11,13 +16,22 @@ public class Main {
          */
         //desplegar valor de los objetos
         
-        auto1.detalle();
+        //auto1.detalle();
         // CReacion de objeto de las flores
         Flores flor1 = new Flores();
-        flor1.color="Rojo";
-        flor1.nombre="Rosa";
-        flor1.precio= 1;
-        flor1.stock= 300;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Ingrese el nombre de la flor :");
+        flor1.nombre = br.readLine();
+        System.out.print("Ingrese el precio de la flor :");
+        flor1.precio= Double.parseDouble(br.readLine());
+        System.out.print("Ingrese el stock de la flor :");
+        flor1.stock= Integer.parseInt(br.readLine());
+        System.out.print("Ingrese el color de la flor :");
+        flor1.color= br.readLine();
+
+        flor1.nombre= JOptionPane.showInputDialog("Nombre de la flor:");
+        JOptionPane.showMessageDialog(null,"El nombre de la flor es:" +flor1.nombre);
+
 
         Flores flor2 = new Flores();
         flor2.color="Amarillo";
