2433e1ed450c9bc09206f68f4a6f777a60c18022
Jaime Hidalgo <jaime.hidalgo.barroso@udla.edu.ec>
2025-10-06 10:20:32 -0500

    Update Flores.java

--- clase semana 1/src/Flores.java
+++ clase semana 1/src/Flores.java
@@ -1,14 +1,25 @@
+import javax.swing.JOptionPane;
+
 public class Flores {
     String color;
     String nombre;
     double precio;
     int stock;
 
-    public void detalle(){
-        System.out.println("Nombre: "+ this.nombre);
-        System.out.println("Color: "+ this.color);
-        System.out.println("Precio: "+ this.precio);
-        System.out.println("Stock: "+ this.stock);
+    public Flores(String color, String nombre, double precio, int stock) {
+        this.color = color;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
     }
+
+    public String detalle(String color){
+        String c;
+
+        JOptionPane.showMessageDialog(null,"COlor: \n"+ color);
+        c = color+"Rojo";
+        return c;
+    }
+
 
 }
