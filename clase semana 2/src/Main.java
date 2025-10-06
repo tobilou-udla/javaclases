00a56a7633df469d5f1aa6c84fb17cafa14efea5
Jaime Hidalgo <jaime.hidalgo.barroso@udla.edu.ec>
2025-10-06 10:57:34 -0500

    Update Main.java

--- clase semana 2/src/Main.java
+++ clase semana 2/src/Main.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Proveedor vendor1= new Proveedor();
+        Scanner sc = new Scanner(System.in);
+        String ruc,rs,dir,dp;
+        System.out.print("Provedor RUC: ");
+        ruc=sc.nextLine();
+        System.out.print("Provedor Razon Social: ");
+        rs=sc.nextLine();
+        System.out.print("Provedor Direccion: ");
+        dir=sc.nextLine();
+        dp= vendor1.detalleProveedor(ruc,rs,dir);
 
+        System.out.println("Los datos son\n"+dp);
+        
     }
 }
